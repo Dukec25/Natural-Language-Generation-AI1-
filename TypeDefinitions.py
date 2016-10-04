fd6416dce825545824b4ae98b08020aa3be6ad30
d87zhang <dzhang323@hotmail.com>
2016-10-03 22:30:35 -0400

    Fix stupid mistake in Node definition

--- TypeDefinitions.py
+++ TypeDefinitions.py
@@ -1,11 +1,11 @@
 class Node:
-    def __init__(wordTuple):
+    def __init__(self, word, posAsInt):
         # (word, pos) tuple, where word is the word as a string
         # and pos is the part of speech as an int (see Utils.PosToInt)
         # in a graph, there should be at most one node with a given
         # (word, pos) tuple
-        wordTuple
+        self.wordTuple = (word, posAsInt)
 
         # list of (node, probability) tuples, where probability 
         # is a float/number from 0 to 1
-        successorList
+        self.successorList = []
