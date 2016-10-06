fe73c6a21b20bc4d6c292f6c3d13b483cebb6c6f
d87zhang <dzhang323@hotmail.com>
2016-10-05 22:53:41 -0400

    Completed DFS search. Didn't test

--- Search.py
+++ Search.py
@@ -1,4 +1,4 @@
-from TypeDefinitions import Node
+from TypeDefinitions import Node, Sequence
 from Utils import PosToInt
 
 import queue
@@ -13,12 +13,41 @@ def generate(startingWord, sentenceSpec, graph):
 
     startNode = nodeMap[(startingWord, sentenceSpecAsInts[0])]
 
-    # represents nodes to explore
-    # each element is a tuple of form (node, idx, )
-    # - idx: index used in sentenceSpec 
+    # represents sequences to explore, contains only sequences that currently
+    # satisfy the sentenceSpec
     exploreQueue = queue.Queue()    
-    exploreQueue.put_nowait(startNode)
+    exploreQueue.put_nowait(Sequence(startNode))
 
+    exploreCount = 0
+    currBestSentence = None
     while not exploreQueue.empty():
-        currNode = exploreQueue.get_nowait()
+        # rather than checking for the goal state when popping from the 
+        # explore data structure, we check before we're about to put it in,
+        # to populate currBestSentence more quickly
+        currSequence = exploreQueue.get_nowait()
+        exploreCount += 1
+        currNode = currSequence.nodes[-1]
+        nextIdxInSentence = currSequence.nodes.count()
+        nextPosAsInt = sentenceSpecAsInts[nextIdxInSentence]
+        nextWordIsLast = (nextIdxInSentence == len(sentenceSpecAsInts) - 1)
+
+        # sanity check
+        if nextIdxInSentence > len(sentenceSpecAsInts) - 1:
+            import pdb; pdb.set_trace()
+            raise Exception("Unexpected! Goal state reached from currSequence!")
+
+        for childNode, edgeProbability in currNode.successorList:
+            if childNode[1] == nextPosAsInt:
+                copiedSequence = currSequence.copy()
+                copiedSequence.addNode(childNode, edgeProbability)
+
+                # check if the resulting sequence has a chance at achieving better
+                # probability
+                if (not currBestSentence) or (copiedSequence.probability > currBestSentence.probability):
+                    if nextWordIsLast:
+                        currBestSentence = copiedSequence
+                    else: # not nextWordIsLast
+                        exploreQueue.put_nowait(copiedSequence)
+                
+
 
--- TypeDefinitions.py
+++ TypeDefinitions.py
@@ -1,3 +1,5 @@
+import copy
+
 class Node:
     def __init__(self, word, posAsInt):
         # (word, pos) tuple, where word is the word as a string
@@ -9,3 +11,20 @@ class Node:
         # list of (node, probability) tuples, where probability 
         # is a float/number from 0 to 1
         self.successorList = []
+
+class Sequence:
+    def __init__(self, startNode):
+        self.nodes = [startNode]
+        self.probability = 1
+
+    def addNode(self, node, edgeProbability):
+        self.nodes.append(node)
+        self.probability *= probability
+
+    def copy(self):
+        """Returns a copy of self, with a copied list pointing to the
+        same Nodes"""
+        theCopy = Sequence(None) # use a temporary startNode
+        theCopy.nodes = copy.copy(self.nodes)
+        theCopy.probability = self.probability
+        return theCopy
