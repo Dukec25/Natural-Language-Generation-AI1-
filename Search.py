f27bd1eae92c8ef1bd0ff22a18838803aaa5dba1
Duke <d.changqi@gmail.com>
2016-10-05 21:08:24 -0400

    heuristic search prototype

--- Search.py
+++ Search.py
@@ -1,6 +1,7 @@
 from TypeDefinitions import Node
 from Utils import PosToInt
 
+import operator
 import queue
 
 def generate(startingWord, sentenceSpec, graph):
@@ -21,4 +22,40 @@ def generate(startingWord, sentenceSpec, graph):
 
     while not exploreQueue.empty():
         currNode = exploreQueue.get_nowait()
+
+
+def getKey(item):
+    return item[1];
+
+""" untested 
+# always choose the word of right type of POS and have the highest possibility
+def heuristic(startNode, sentenceSpec, graph):
+    index = 1; # current word 
+    "sort the list so highest probabilities goes first"
+    result = [];
+    result.append(startNode.wordTuple[0]);
+    probability = 1;
+    currentNode = startNode;
+    success = False; 
+
+    while( currentNode != None ) 
+        sorted(startNode.successorList, key=getKey, reverse=true); 
+        for pairedElement in startNode.successorList:
+            if pairedElement.wordTuple[1] == sentenceSpec[index]:
+                    if (index == len(sentenceSpec) -1):
+                        success = True;
+                        currentNode = None;
+                        break; 
+                    index = index + 1;
+                    result.append(" " + pairedElement.wordTuple[0]);
+                    probability = probability * currentNode.successorList[1];
+                    currentNode = pairedElement; 
+                    continue;
+        break; 
+
+    if (success):
+        #print out stuff
+    else
+        #print out stuff 
+"""
 
