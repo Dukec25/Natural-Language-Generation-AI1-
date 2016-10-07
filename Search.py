94e88a19d3a03d43250aed0c5daeceed7a362109
d87zhang <dzhang323@hotmail.com>
2016-10-07 01:04:10 -0400

    Added heuristic search and corresponding results

--- Search.py
+++ Search.py
@@ -4,11 +4,78 @@ import Parser
 
 import queue
 from collections import deque
+from heapq import *
+
+def heuristic_generate(startingWord, sentenceSpec, graph):
+    """Does the same thing as generate, but with the heuristic search strategy.
+
+    Our heuristic function h(n) = n's current probability (n is a sequence)
+    Nodes with a higher valued h(n) are preferred and are searched first
+    """
+    # much of the code is copied from generate()...
+    nodeMap = Parser.parse(graph)
+
+    sentenceSpecAsInts = [PosToInt(pos) for pos in sentenceSpec]
+
+    startNode = nodeMap[(startingWord, sentenceSpecAsInts[0])]
+
+    # ideally we use a heap/priority-queue to find the sequence with the highest
+    # heuristic function value, but implementing that turned out to be difficult
+    # Instead, we will just use a list to store the sequences to explore and
+    # everytime we pick from the list, we find the one that has the highest
+    # heuristic function value
+    exploreStructure = []
+    exploreStructure.append(Sequence(startNode))
+
+    def heuristic_function(sequence):
+        return sequence.probability
+
+    exploreCount = 0
+    currBestSentence = None
+    while exploreStructure: # not empty
+        # rather than checking for the goal state when popping from the 
+        # explore data structure, we check before we're about to put it in,
+        # to populate currBestSentence more quickly
+
+        currSequence = max(exploreStructure, key=heuristic_function)
+        exploreStructure.remove(currSequence)
+
+        exploreCount += 1
+        currNode = currSequence.nodes[-1]
+        nextIdxInSentence = len(currSequence.nodes)
+        nextPosAsInt = sentenceSpecAsInts[nextIdxInSentence]
+        nextWordIsLast = (nextIdxInSentence == len(sentenceSpecAsInts) - 1)
+
+        # sanity check
+        if nextIdxInSentence > len(sentenceSpecAsInts) - 1:
+            import pdb; pdb.set_trace()
+            raise Exception("Unexpected! Goal state reached from currSequence!")
+
+        for childNode, edgeProbability in currNode.successorList:
+            if childNode.wordTuple[1] == nextPosAsInt:
+                copiedSequence = currSequence.copy()
+                copiedSequence.addNode(childNode, edgeProbability)
+
+                # check if the resulting sequence has a chance at achieving better
+                # probability
+                if (not currBestSentence) or (copiedSequence.probability > currBestSentence.probability):
+                    if nextWordIsLast:
+                        currBestSentence = copiedSequence
+                    else: # not nextWordIsLast
+                        exploreStructure.append(copiedSequence)
+          
+    outputString = '"' + currBestSentence.getString() + '"' \
+                   + " with probability " + str(currBestSentence.probability)
+    print(outputString)
+    print("Total nodes considered: ", exploreCount)
 
 def generate(startingWord, sentenceSpec, graph, searchStrategy='BFS'):
     """Prints a string (not necessarily one line) with the following information:
     1. the highest-probability sentence found
     2. the number of nodes (i.e. word sequences) considerd during the search"""
+    if searchStrategy=='heuristic':
+        return heuristic_generate(startingWord, sentenceSpec, graph)
+
     nodeMap = Parser.parse(graph)
 
     sentenceSpecAsInts = [PosToInt(pos) for pos in sentenceSpec]
@@ -73,7 +140,7 @@ def test(filename="input.txt"):
             string += ", '" + spec + "'"
         return string
 
-    searchStrategy = 'DFS'
+    searchStrategy = 'heuristic'
 
     print("====================================")
     sentenceSpec = ['NNP', 'VBD', 'DT', 'NN']
